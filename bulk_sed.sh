996087d33be67567beb814cb985d8dd92e466d63
AJ Greyling <agreyling@mezzanineware.com>
2018-06-05 10:36:34 +0200

    no message

--- bulk_sed.sh
+++ bulk_sed.sh
@@ -2,7 +2,7 @@
 mkdir $2
 IFS=$'\0'
 while IFS= read -r -d '' p; do
- filename=no_schema/$(basename $p)
+ filename=$2/$(basename $p)
  echo $filename
  #cat $p  | sed 's/\-\-.*//g;s/search_path.*;//g;s/the_schema.*;//g;s/ndoh_stock_002\.//g;s/zambia__svs_001\.//g' > $filename
  cat $p  | sed $3 > $filename 
